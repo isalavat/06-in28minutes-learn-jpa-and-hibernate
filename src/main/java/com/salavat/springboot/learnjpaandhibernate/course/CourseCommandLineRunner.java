a2bf6c311a58798ec16e498f5a253766cb673eda
Salavat <salavat.islamov.96@gmail.com>
2024-02-19 20:31:30 +0100

    crud operatins using jakartas jpa

---
+++ src/main/java/com/salavat/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -0,0 +1,33 @@
+package com.salavat.springboot.learnjpaandhibernate.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.salavat.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
+import com.salavat.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+@Component
+public class CourseCommandLineRunner implements CommandLineRunner{
+	
+//	@Autowired
+//	private CourseJdbcRepository repository; 
+	
+	@Autowired
+	private CourseJpaRepository repository; 
+	
+	@Override
+	public void run(String... args) throws Exception {
+	
+		repository.insert(new Course(1, "Learn AWS 1 JPA", "in28minutes"));
+		repository.insert(new Course(2, "Learn Azure Now JPA", "in28minutes"));
+		repository.insert(new Course(3, "Learn DevOps Now JPA", "in28minutes"));
+		
+		repository.deleteById(1);
+		
+		System.out.println(repository.findById(2));
+		System.out.println(repository.findById(3));
+		
+		
+	}
+
+}
